b62f8f70716dab168f67a266b29c26890c9d4e62
markusohlsson <makkeytv@gmail.com>
2024-07-03 21:54:02 +0200

    Removed unused files

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,10 +1,5 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
 import Hero from '../components/Hero.vue'
-import About from '../components/About.vue'
-import Bigardar from '../components/Bigardar.vue'
-
-import Products from '../components/Products.vue'
 </script>
 
 <template>
